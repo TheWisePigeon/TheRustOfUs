2136bc833674c4fbe0b9fd6033b88931d73cef56
TheWisePigeon <zozozozeph@gmail.com>
2022-10-19 11:55:45 +0000

    feat: starting serious shit

--- e1/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-    let japan = "Li hao combi";
-    let espagnol = "Hola amigo";
-    let countries = [japan, espagnol];
-    for country in countries.iter() {
-        println!("{}", &country);
-    }
-}
--- e1/target/debug/e1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\TheWisePigeon\Documents\TheRustOfUs\e1\target\debug\e1.exe: C:\Users\TheWisePigeon\Documents\TheRustOfUs\e1\src\main.rs
