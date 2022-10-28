59754f200ac6c66540743504a36eb5d0b26890b9
TheWisePigeon <zozozozeph@gmail.com>
2022-10-28 21:31:33 +0000

    feat: pusing p

--- rocket_gang/src/main.rs
+++ rocket_gang/src/main.rs
@@ -1,14 +1,15 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{ Build, Rocket };
 
 #[get("/")]
-fn index() -> &'static str  {
+async fn index() -> &'static str  {
     "Hello Rocket Gang"
 }
 
-#[launch]
-fn rocket() -> Rocket<Build>{
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() {
+    rocket::build().mount("/", routes![
+        index
+    ]).launch().await;
 }
