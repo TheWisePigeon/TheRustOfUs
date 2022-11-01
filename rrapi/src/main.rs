75607059752132600fd93390a0eb392d29692dde
TheWisePigeon <zozozozeph@gmail.com>
2022-11-01 23:58:41 +0000

    feat: added println

--- rrapi/src/main.rs
+++ rrapi/src/main.rs
@@ -1,4 +1,13 @@
+use std::io::stdin;
+use rand::Rng;
+
 
 fn main() {
-    println!("okok")
+    println!("Welcome to the guessing game");
+    let mut user_guess = String::new();
+    //Define a variable to generate the random number
+    let mut randomNumber = rand::thread_rng().gen_range(0..10);
+    println!("Enter your guess");
+    stdin().read_line(&mut user_guess);
+    println!("{}", user_guess);
 }
