e3cdc8e17722b23647228b7ff21a5feb011ee9c1
TheWisePigeon <zozozozeph@gmail.com>
2022-11-02 10:19:18 +0000

    feat: made a guessing game

--- rrapi/src/main.rs
+++ rrapi/src/main.rs
@@ -4,10 +4,31 @@ use rand::Rng;
 
 fn main() {
     println!("Welcome to the guessing game");
-    let mut user_guess = String::new();
-    //Define a variable to generate the random number
-    let mut randomNumber = rand::thread_rng().gen_range(0..10);
-    println!("Enter your guess");
-    stdin().read_line(&mut user_guess);
-    println!("{}", user_guess);
+    let random_number = rand::thread_rng().gen_range(0..5);
+    let reader = stdin();
+    let mut success = false;
+    while !success {
+        
+        let mut raw_input: String = String::new();
+        println!("Enter your guess");
+        reader.read_line(&mut raw_input).unwrap();
+        let conversion_result = raw_input.trim().parse::<i32>();
+        match conversion_result {
+            Ok(i)=>{
+                if i == random_number {
+                    println!("Congratulations!");
+                    success = true
+                }
+                if i > random_number{
+                    println!("It's lower")
+                }
+                if i < random_number{
+                    println!("It's higher")
+                }
+            },
+            Err(_)=>{
+                println!("You did not enter a valid integer")
+            }
+        }
+    }
 }
