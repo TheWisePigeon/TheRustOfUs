9c349b76e3b23ac1ff3815b7133e9b3f40aced24
TheWisePigeon <zozozozeph@gmail.com>
2022-11-20 09:47:25 +0000

    duh

--- cheat/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn get_arguments() -> Vec<String> {
-    std::env::args().collect()
-}
-
-fn main() {
-    println!("Hello, world!");
-    let args = get_arguments();
-    println!("{:?}", args);
-}
