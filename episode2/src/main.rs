46801d49f32d815cb406eb5ea5804e19e6b5d60a
TheWisePigeon <zozozozeph@gmail.com>
2022-12-07 23:37:42 +0000

    pushing p

--- episode2/src/main.rs
+++ episode2/src/main.rs
@@ -1,3 +1,20 @@
+
+struct Server{
+    address: String
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Self { 
+            address 
+        }
+    }
+
+    fn run(self) {
+        println!("I am running baby");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
