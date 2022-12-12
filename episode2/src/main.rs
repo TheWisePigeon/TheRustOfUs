0495498b0d5907a77454ac67ea88dc149cc76f17
TheWisePigeon <zozozozeph@gmail.com>
2022-12-12 18:05:54 +0000

    parsed request

--- episode2/src/main.rs
+++ episode2/src/main.rs
@@ -1,12 +1,11 @@
 use server::Server;
-use http::{Method, Request };
+// use http::{Method, Request };
 
 mod server;
 mod http;
 
 
 fn main() {
-    println!("Hello, world!");
-    let test = Server::new("127.0.0:5000".to_string());
+    let test = Server::new("127.0.0.1:5000".to_string());
     test.run();
 }
--- episode2/src/server.rs
+++ episode2/src/server.rs
@@ -1,4 +1,4 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener}, io::Read};
 pub struct Server {
     address: String,
 }
@@ -9,17 +9,26 @@ impl Server {
     }
 
     pub fn run(self) {
-        println!("I am running baby! On {}", self.address);
+        println!("I am running on {} baby", self.address);
 
         let listener = TcpListener::bind(&self.address).unwrap();
 
         loop {
             match listener.accept() {
-                Ok((Stream, SocketAddress)) => {
-
+                Ok((mut stream, _)) => {
+                    let mut buf = [0; 1024];
+                    match stream.read( &mut buf) {
+                        Ok(v) =>{
+                            println!("{}", v);
+                            println!("{}", String::from_utf8_lossy(&buf));
+                        },
+                        Err(e)=>{
+                            println!("Failed to read from socket {}", e);
+                        }
+                    } ;
                 },
                 Err(e) =>  println!("Failed to accepts incoming connection {}", e),
-            }
+            };
         }
     }
 }
